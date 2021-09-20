ab928e5ed98b8dc08b051a2e1ced08387c3940da
dai-ichiro <d.a.i-ichiro@outlook.jp>
2021-09-20 11:35:25 +0900

    update code

--- reversi/Program.cs
+++ reversi/Program.cs
@@ -1,11 +1,22 @@
-﻿namespace reversi;
+﻿using System.Diagnostics;
+
+namespace reversi;
 
 class Program
 {
     static void Main(string[] args)
     {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
         Search search = new Search();
         search.start_search();
+
+        sw.Stop();
+
+        TimeSpan ts = new TimeSpan();
+        ts = sw.Elapsed;
+        Console.WriteLine("Finished!({0})", ts);
     }
 }
 
